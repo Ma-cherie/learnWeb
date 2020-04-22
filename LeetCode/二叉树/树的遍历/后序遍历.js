134ecf7e09937b3312db67477a2c882888f2f7f0
ma-cherie <240710290@qq.com>
2020-04-22 17:02:54 +0800

    删除LeetCode文件夹 题目会继续做，之前的小白解法就没必要记录下来了...会做的题再做一遍也是会的

--- LeetCode/二叉树/树的遍历/后序遍历.js
+++
@@ -1,8 +0,0 @@
-// 递归
-var postorderTraversal = function (root, arr = []) {
-    if (root == null) return [];
-    postorderTraversal(root.left, arr);
-    postorderTraversal(root.right, arr);
-    arr.push(root.val)
-    return arr;
-};
