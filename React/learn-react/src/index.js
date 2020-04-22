6366bf7344821d48f4974f75cc890a8de0b29cda
ma-cherie <240710290@qq.com>
2020-04-22 17:02:07 +0800

    开坑React

---
+++ React/learn-react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const h1 = (
+    <React.Fragment>
+        <h1>Hello React</h1>
+        
+    </React.Fragment>
+)
+
+ReactDOM.render(h1,document.getElementById('root'))
